3f91d4869be087be5d54249b42013bb1b446c092
httvinh <86193698+httvinh@users.noreply.github.com>
2021-06-30 17:39:05 +0700

    update ex2 week2

--- src/exercises_02/hibernate_annotations/src/test/java/tma/hibernate/ApplicationTests.java
+++ src/exercises_02/hibernate_annotations/src/test/java/tma/hibernate/ApplicationTests.java
@@ -1,10 +1,10 @@
-package tma.demo2;
+package tma.hibernate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Demo2ApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
